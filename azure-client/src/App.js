0a01d972af091c4ea1e42a9e3c17dbb8969ecd63
katemariv <89603758+katemariv@users.noreply.github.com>
2021-08-28 16:15:01 -0600

    feat: Add Table and read from Azure using Axios

--- azure-client/src/App.js
+++ azure-client/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { Layout, Menu } from 'antd';
+import { CloudServerOutlined } from '@ant-design/icons';
 import './App.css';
+import './extraStyle.scss';
+import 'antd/dist/antd.css';
+import ProductTable from './components/productTable';
 
 function App() {
+  const REACT_APP_SERVER_NAME = process.env.REACT_APP_SERVER_NAME || 'No Server specified';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout className="layout">
+        <Layout.Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="mail" icon={<CloudServerOutlined />}>
+              {REACT_APP_SERVER_NAME}
+            </Menu.Item>
+          </Menu>
+        </Layout.Header>
+
+        <Layout.Content style={{ padding: '0 50px' }}>
+          <div className="site-layout-content"><ProductTable/></div>
+        </Layout.Content>
+
+        <Layout.Footer style={{ textAlign: 'center' }}>Arquitectura de Redes y Sistemas Empresariales ©2021</Layout.Footer>
+      </Layout>
     </div>
   );
 }
---
+++ azure-client/src/components/productTable/index.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import { Table, Tag, Typography } from 'antd';
+import Axios from "axios";
+
+
+const ProductTable = () => {
+    const [state, setstate] = useState([]);
+    const [loading, setloading] = useState(true);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        await Axios.get(process.env.REACT_APP_API_URL).then(
+            res => {
+                setloading(false);
+                setstate(
+                    res.data.map(row => ({
+                        Name: row.Name,
+                        ProductNumber: row.ProductNumber,
+                        Color: row.Color,
+                        StandardCost: row.StandardCost,
+                        ListPrice: row.ListPrice,
+                    }))
+                );
+            }
+        );
+    };
+
+    const columns = [
+        {
+            title: 'Product Name',
+            dataIndex: 'Name',
+            key: 'Name',
+            render: text => <Typography.Text strong>{text}</Typography.Text>,
+        },
+        {
+            title: 'Product Number',
+            dataIndex: 'ProductNumber',
+            key: 'ProductNumber',
+        },
+        {
+            title: 'Color',
+            dataIndex: 'Color',
+            key: 'Color',
+            render: color => {
+                let tagColor = '';
+                let arrTags = [];
+
+                if (color) {
+                    arrTags = color.split("/").map(item => {
+                        switch (item) {
+                            case "Red":
+                                tagColor = "red";
+                                break;
+                            case "Multi":
+                                tagColor = "magenta";
+                                break;
+                            case "Silver":
+                            case "Grey":
+                                tagColor = "geekblue";
+                                break;
+                            case "Yellow":
+                                tagColor = "gold";
+                                break;
+                            case "Blue":
+                                tagColor = "blue";
+                                break;
+                            default:
+                                tagColor = "default";
+                        }
+
+                        return <Tag color={tagColor}>{item}</Tag>;
+                    });
+                }
+
+                return arrTags;
+            }
+        },
+        {
+            title: 'Standard Cost',
+            dataIndex: 'StandardCost',
+            key: 'StandardCost',
+        },
+        {
+            title: 'List Price',
+            dataIndex: 'ListPrice',
+            key: 'ListPrice',
+        },
+    ];
+
+    return <Table columns={columns} dataSource={state} loading={loading} />;
+}
+
+export default ProductTable;
