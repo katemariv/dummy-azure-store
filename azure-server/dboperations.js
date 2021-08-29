a9849197fcbdd9bb69f0aa240095351bce3cd408
katemariv <89603758+katemariv@users.noreply.github.com>
2021-08-28 20:38:45 -0600

    refactor: Remove Azure 1 flag

--- azure-server/dboperations.js
+++ azure-server/dboperations.js
@@ -1,13 +1,13 @@
-var USE_AZURE = process.env.USE_AZURE || 0;
+// var USE_AZURE = process.env.USE_AZURE || 0;
 let QUERY_STR = "";
 
-if(USE_AZURE == 1){
-    var config = require('./dbconfigazure');
-    QUERY_STR = "SELECT ProductID, Name, ProductNumber, Color, StandardCost, ListPrice from bddTest.SalesLT.Product";
-}else{
+// if(USE_AZURE == 1){
+var config = require('./dbconfigazure');
+QUERY_STR = "SELECT ProductID, Name, ProductNumber, Color, StandardCost, ListPrice from bddTest.SalesLT.Product";
+/* }else{
     var config = require('./dbconfig');
     QUERY_STR = "SELECT * from Products";
-}
+} */
 
 const sql = require('mssql');
 
